5c89b9b18ba13ce7ad97200a97907dd924c12ce2
David Tang <dtang85@gmail.com>
2012-06-14 12:28:44 -0700

    updated html and php in template

--- wp-content/themes/client-child-theme/survey-form.php
+++ wp-content/themes/client-child-theme/survey-form.php
@@ -4,7 +4,11 @@
  */
 ?>
 
-<?php get_header(); ?>
+<?php 
+	get_header(); 
+
+?>
+	<form action="<?php the_permalink(); ?>" method="POST">
 
 <?php
 	$args = array(
@@ -19,11 +23,9 @@
 	//print_r(get_post_meta($post_id));
 	//print_r(get_post_custom($post_id));
 	$more_fields = get_post_custom($post_id);
-
 	while ( $the_query->have_posts() ) : $the_query->the_post();
 ?>
 
-<form action="" method="POST">
 	<div class="question"><?php the_title(); ?></div>
 
 <?php foreach ($more_fields as $key => $value) {
@@ -32,12 +34,12 @@
 	$image_key = 'image' . $matches[0];
 	$title = $more_fields[$key][0];
 ?>
-	<div class="question">
+	<div class="question-options">
 		<input type="radio" name="options" value="<?php echo $title; ?>" id="<?php echo $title; ?>" />
 		<label for="<?php echo $title; ?>">
 			<span><?php echo $title; ?></span>
 			<img src="<?php echo $more_fields[$image_key][0]; ?>" />
-	</label>
+		</label>
 	</div>
 	
 <?php endif; } ?>
