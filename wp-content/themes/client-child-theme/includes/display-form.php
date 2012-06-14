1c6c809697ff4c58792d76408ac140e3af727742
David Tang <dtang85@gmail.com>
2012-06-14 12:40:45 -0700

    updated html. separated form submission logic

---
+++ wp-content/themes/client-child-theme/includes/display-form.php
@@ -0,0 +1,44 @@
+<div class="survey">
+<form action="<?php the_permalink(); ?>" method="POST">
+
+<?php
+	$args = array(
+		'post_status' => 'publish',
+		'category_name' => 'questions',
+		'posts_per_page' => 1
+	);
+	$the_query = new WP_Query($args);
+	$post_id = $the_query->posts[0]->ID;
+	//print_r($the_query->posts[0]->ID);
+	//print_r($the_query);
+	//print_r(get_post_meta($post_id));
+	//print_r(get_post_custom($post_id));
+	$more_fields = get_post_custom($post_id);
+	while ( $the_query->have_posts() ) : $the_query->the_post();
+?>
+
+	<div class="question"><?php the_title(); ?></div>
+
+<?php foreach ($more_fields as $key => $value) {
+	if (strpos($key, 'label') !== FALSE) :
+	$keyNumeric = preg_match('/\d{1,2}/', $key, $matches);
+	$image_key = 'image' . $matches[0];
+	$title = $more_fields[$key][0];
+?>
+
+	<div class="question-options">
+		<input type="radio" name="options" value="<?php echo $title; ?>" id="<?php echo $title; ?>" />
+		<label for="<?php echo $title; ?>">
+			<span><?php echo $title; ?></span>
+			<img src="<?php echo $more_fields[$image_key][0]; ?>" />
+		</label>
+	</div>
+	
+<?php endif; } ?>
+	<input type="submit" value="Vote" name="vote" />
+</form>
+
+<?php endwhile;
+	wp_reset_postdata();
+?>
+</div>
---
+++ wp-content/themes/client-child-theme/includes/process-submit.php
@@ -0,0 +1,11 @@
+<?php
+if(isset($_POST['options'])) {
+	$option = $_POST['options'];
+	echo $option;
+}
+
+
+
+?>
+
+<div class="thankyou">Thank you for your vote</div>
